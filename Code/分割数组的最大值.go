fe0f17d4cc33a08f257d5811285bc62a0070a604
JianQi <dingjianqi98@foxmail.com>
2023-05-23 10:30:15 +0800

    410 百度

--- Code/分割数组的最大值.go
+++ Code/分割数组的最大值.go
@@ -1,5 +1,58 @@
 package Code
 
-func splitArray(nums []int, k int) int {
+import "fmt"
 
+func splitArray(nums []int, k int) int {
+	i := max(nums)
+	j := sum(nums)
+	res := j
+	for i <= j {
+		mid := (i + j) / 2
+		split := split_num(nums, mid)
+		if split <= k {
+			if mid < res {
+				res = mid
+			}
+			j = mid - 1
+		} else if split > k {
+			i = mid + 1
+		}
+	}
+	return res
+}
+func split_num(nums []int, total int) int {
+	if len(nums) < 1 {
+		return 0
+	}
+	k := 0
+	res := 1
+	for _, v := range nums {
+		if k+v <= total {
+			k += v
+		} else {
+			k = v
+			res++
+		}
+	}
+	return res
+}
+func sum(nums []int) int {
+	res := 0
+	for _, v := range nums {
+		res += v
+	}
+	return res
+}
+func max(nums []int) int {
+	res := nums[0]
+	for _, v := range nums {
+		if v > res {
+			res = v
+		}
+	}
+	return res
+}
+
+func main() {
+	fmt.Println(splitArray([]int{1, 2, 3, 4, 5}, 2))
 }
